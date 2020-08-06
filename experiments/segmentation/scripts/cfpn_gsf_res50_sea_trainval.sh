0661fbcefb45cf143fa9dbdf93339d24ea353aca
yougoforward <908865817@qq.com>
2020-08-06 22:09:00 +0800

    sea init

---
+++ experiments/segmentation/scripts/cfpn_gsf_res50_sea_trainval.sh
@@ -0,0 +1,15 @@
+# !/usr/bin/env bash
+# train
+python -m experiments.segmentation.train --dataset sea --train_split trainval \
+    --model cfpn_gsf --aux --base-size 520 --crop-size 520 \
+    --backbone resnet50 --checkname cfpn_gsf_res50_sea
+
+#test [single-scale]
+python -m experiments.segmentation.test_whole --dataset sea \
+    --model cfpn_gsf --aux --base-size 520 --crop-size 520 \
+    --backbone resnet50 --resume experiments/segmentation/runs/sea/cfpn_gsf/cfpn_gsf_res50_sea/model_best.pth.tar --split val --mode testval
+
+#test [multi-scale]
+python -m experiments.segmentation.test_whole --dataset sea \
+    --model cfpn_gsf --aux --base-size 520 --crop-size 520 \
+    --backbone resnet50 --resume experiments/segmentation/runs/sea/cfpn_gsf/cfpn_gsf_res50_sea/model_best.pth.tar --split val --mode testval --ms
