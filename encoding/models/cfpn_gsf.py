f995d3a86d375b706fbe16f07379281d7cbb80ee
yougoforward <908865817@qq.com>
2020-08-06 20:37:02 +0800

    sea init

--- encoding/models/cfpn_gsf.py
+++ encoding/models/cfpn_gsf.py
@@ -51,7 +51,7 @@ class cfpn_gsfHead(nn.Module):
         self.se = nn.Sequential(
                             nn.Conv2d(inter_channels, inter_channels, 1, bias=True),
                             nn.Sigmoid())
-        self.gff = PAM_Module(in_dim=inter_channels, key_dim=inter_channels//8,value_dim=inter_channels,out_dim=inter_channels,norm_layer=norm_layer)
+        self.gff = PAM_Module(in_dim=inter_channels, key_dim=64,value_dim=inter_channels,out_dim=inter_channels,norm_layer=norm_layer)
 
         self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
 
