78c8bdd6d7110aef29539471d5c78eb4f7295389
yougoforward <908865817@qq.com>
2020-08-07 12:04:09 +0800

    sea init

--- experiments/segmentation/infer.py
+++ experiments/segmentation/infer.py
@@ -21,7 +21,48 @@ from encoding.models import get_model, get_segmentation_model
 from encoding.models import MultiEvalModule_whole as MultiEvalModule
 from .option import Options
 import numpy as np
+def batch_intersection_union(output, target, nclass):
+    """Batch Intersection of Union
+    Args:
+        predict: input 4D tensor
+        target: label 3D tensor
+        nclass: number of categories (int)
+    """
+    _, predict = torch.max(output, 1)
+    mini = 1
+    maxi = nclass
+    nbins = nclass
+    predict = predict.cpu().numpy().astype('int64') + 1
+    target = target.cpu().numpy().astype('int64') + 1
 
+    predict = predict * (target > 0).astype(predict.dtype)
+    intersection = predict * (predict == target)
+    # areas of intersection and union
+    area_inter, _ = np.histogram(intersection, bins=nbins, range=(mini, maxi))
+    area_pred, _ = np.histogram(predict, bins=nbins, range=(mini, maxi))
+    area_lab, _ = np.histogram(target, bins=nbins, range=(mini, maxi))
+    area_union = area_pred + area_lab - area_inter
+    assert (area_inter <= area_union).all(), \
+        "Intersection area should be smaller than Union area"
+    return area_inter, area_union, area_pred, area_lab
+
+
+def batch_pix_accuracy(output, target):
+    """Batch Pixel Accuracy
+    Args:
+        predict: input 4D tensor
+        target: label 3D tensor
+    """
+    _, predict = torch.max(output, 1)
+
+    predict = predict.cpu().numpy().astype('int64') + 1
+    target = target.cpu().numpy().astype('int64') + 1
+
+    pixel_labeled = np.sum(target > 0)
+    pixel_correct = np.sum((predict == target)*(target > 0))
+    assert pixel_correct <= pixel_labeled, \
+        "Correct area should be smaller than Labeled"
+    return pixel_correct, pixel_labeled
 def test(args):
     # output folder
     outdir = args.save_folder
@@ -124,45 +165,3 @@ if __name__ == "__main__":
     test(args)
 
 
-def batch_intersection_union(output, target, nclass):
-    """Batch Intersection of Union
-    Args:
-        predict: input 4D tensor
-        target: label 3D tensor
-        nclass: number of categories (int)
-    """
-    _, predict = torch.max(output, 1)
-    mini = 1
-    maxi = nclass
-    nbins = nclass
-    predict = predict.cpu().numpy().astype('int64') + 1
-    target = target.cpu().numpy().astype('int64') + 1
-
-    predict = predict * (target > 0).astype(predict.dtype)
-    intersection = predict * (predict == target)
-    # areas of intersection and union
-    area_inter, _ = np.histogram(intersection, bins=nbins, range=(mini, maxi))
-    area_pred, _ = np.histogram(predict, bins=nbins, range=(mini, maxi))
-    area_lab, _ = np.histogram(target, bins=nbins, range=(mini, maxi))
-    area_union = area_pred + area_lab - area_inter
-    assert (area_inter <= area_union).all(), \
-        "Intersection area should be smaller than Union area"
-    return area_inter, area_union, area_pred, area_lab
-
-
-def batch_pix_accuracy(output, target):
-    """Batch Pixel Accuracy
-    Args:
-        predict: input 4D tensor
-        target: label 3D tensor
-    """
-    _, predict = torch.max(output, 1)
-
-    predict = predict.cpu().numpy().astype('int64') + 1
-    target = target.cpu().numpy().astype('int64') + 1
-
-    pixel_labeled = np.sum(target > 0)
-    pixel_correct = np.sum((predict == target)*(target > 0))
-    assert pixel_correct <= pixel_labeled, \
-        "Correct area should be smaller than Labeled"
-    return pixel_correct, pixel_labeled
