fbc37560de800565507cad11bd198336b7119147
yougoforward <908865817@qq.com>
2020-08-07 14:23:54 +0800

    sea init

--- experiments/segmentation/scripts/infer2.sh
+++ experiments/segmentation/scripts/infer2.sh
@@ -1,5 +1,5 @@
 # !/usr/bin/env bash
 #test [single-scale]
 python -m experiments.segmentation.infer2 --dataset sea \
-    --model cfpn --base-size 456 --crop-size 456 \
+    --model cfpn --base-size 513 --crop-size 513 \
     --backbone resnet50 --resume experiments/segmentation/runs/sea/cfpn/cfpn_res50_sea/model_best.pth.tar --split val --mode testval
