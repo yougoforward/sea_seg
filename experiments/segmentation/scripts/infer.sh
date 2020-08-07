267a17e083912cd2bf2f78d55df5a5ced393371f
yougoforward <908865817@qq.com>
2020-08-07 11:45:40 +0800

    sea init

--- experiments/segmentation/scripts/infer.sh
+++ experiments/segmentation/scripts/infer.sh
@@ -2,5 +2,5 @@
 #test [single-scale]
 python -m experiments.segmentation.infer --dataset sea \
     --model cfpn_gsf --base-size 321 --crop-size 321 \
-    --backbone resnet50 --resume experiments/segmentation/runs/sea/cfpn_gsf/cfpn_gsf_res50_sea/model_best.pth.tar --split test --mode test \
+    --backbone resnet50 --resume experiments/segmentation/runs/sea/cfpn_gsf/cfpn_gsf_res50_sea/model_best.pth.tar --split val --mode testval \
     --save-folder experiments/segmentation/results/cfpn_gsf_res50_sea
