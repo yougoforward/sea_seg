74546a0c0ac59df74ddf9b175c3fa0fb814d4b48
yougoforward <908865817@qq.com>
2020-08-07 08:25:50 +0800

    sea init

--- experiments/segmentation/test_whole.py
+++ experiments/segmentation/test_whole.py
@@ -76,26 +76,26 @@ def test(args):
                 pixAcc, mIoU = metric.get()
                 tbar.set_description( 'pixAcc: %.4f, mIoU: %.4f' % (pixAcc, mIoU))
         else:
-            # with torch.no_grad():
-            #     outputs = evaluator.parallel_forward(image)
-            #     predicts = [testset.make_pred(torch.max(output, 1)[1].cpu().numpy())
-            #                 for output in outputs]
-            # for predict, impath in zip(predicts, dst):
-            #     mask = utils.get_mask_pallete(predict, args.dataset)
-            #     outname = os.path.splitext(impath)[0] + '.png'
-            #     mask.save(os.path.join(outdir, outname))
             with torch.no_grad():
                 outputs = evaluator.parallel_forward(image)
-                # predicts = [testset.make_pred(torch.max(output, 1)[1].cpu().numpy())
-                #             for output in outputs]
-                predicts = [torch.softmax(output,1).cpu().numpy() for output in outputs]
+                predicts = [testset.make_pred(torch.max(output, 1)[1].cpu().numpy())
+                            for output in outputs]
             for predict, impath in zip(predicts, dst):
-                # mask = utils.get_mask_pallete(predict, args.dataset)
-                import numpy as np
-                from PIL import Image
-                mask = Image.fromarray((predict[0,1,:,:]*255).astype(np.uint8))
-                outname = os.path.splitext(impath)[0] + '.bmp'
+                mask = utils.get_mask_pallete(predict, args.dataset)
+                outname = os.path.splitext(impath)[0] + '.png'
                 mask.save(os.path.join(outdir, outname))
+            # with torch.no_grad():
+            #     outputs = evaluator.parallel_forward(image)
+            #     # predicts = [testset.make_pred(torch.max(output, 1)[1].cpu().numpy())
+            #     #             for output in outputs]
+            #     predicts = [torch.softmax(output,1).cpu().numpy() for output in outputs]
+            # for predict, impath in zip(predicts, dst):
+            #     # mask = utils.get_mask_pallete(predict, args.dataset)
+            #     import numpy as np
+            #     from PIL import Image
+            #     mask = Image.fromarray((predict[0,1,:,:]*255).astype(np.uint8))
+            #     outname = os.path.splitext(impath)[0] + '.bmp'
+            #     mask.save(os.path.join(outdir, outname))
 if __name__ == "__main__":
     args = Options().parse()
     torch.manual_seed(args.seed)
