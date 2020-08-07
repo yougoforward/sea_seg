8dbf687ef0146a33f23560caaeab7d6da5e9578e
yougoforward <908865817@qq.com>
2020-08-07 08:37:13 +0800

    sea init

--- experiments/segmentation/scripts/cfpn_gsf_res50_sea_trainval.sh
+++ experiments/segmentation/scripts/cfpn_gsf_res50_sea_trainval.sh
@@ -1,21 +1,21 @@
 # !/usr/bin/env bash
 # train
 python -m experiments.segmentation.train --dataset sea --train-split trainval \
-    --model cfpn_gsf --aux --base-size 520 --crop-size 520 \
+    --model cfpn_gsf --aux --base-size 321 --crop-size 321 \
     --backbone resnet50 --checkname cfpn_gsf_res50_sea
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset sea \
-    --model cfpn_gsf --aux --base-size 520 --crop-size 520 \
+    --model cfpn_gsf --aux --base-size 321 --crop-size 321 \
     --backbone resnet50 --resume experiments/segmentation/runs/sea/cfpn_gsf/cfpn_gsf_res50_sea/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset sea \
-    --model cfpn_gsf --aux --base-size 520 --crop-size 520 \
+    --model cfpn_gsf --aux --base-size 321 --crop-size 321 \
     --backbone resnet50 --resume experiments/segmentation/runs/sea/cfpn_gsf/cfpn_gsf_res50_sea/model_best.pth.tar --split val --mode testval --ms
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset sea \
-    --model cfpn_gsf --aux --base-size 520 --crop-size 520 \
+    --model cfpn_gsf --aux --base-size 321 --crop-size 321 \
     --backbone resnet50 --resume experiments/segmentation/runs/sea/cfpn_gsf/cfpn_gsf_res50_sea/model_best.pth.tar --split test --mode test \
     --save-folder experiments/segmentation/results/cfpn_gsf_res50_sea
